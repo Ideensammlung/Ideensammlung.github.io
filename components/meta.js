7d6bb7c5d5455efdbfd4c69137017def9101075f
Ideensammlung <jgs.salomon+github.com+ideensammlung@gmail.com>
2021-10-18 09:00:13 +0200

    add google analytics

--- components/meta.js
+++ components/meta.js
@@ -4,6 +4,19 @@ export default function Meta() {
   return (
     <Head>
       <title>Ideensammlung</title>
+
+      {/* Global site tag (gtag.js) - Google Analytics */ ''}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-W1NLWDJEV1"></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-W1NLWDJEV1');
+          `,
+        }}
+      />
     </Head>
   )
 }
